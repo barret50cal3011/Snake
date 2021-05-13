dbd6eb93dfc2b0edf5657b512781f8da4408df03
B4RRET <barret50cal3011@gmail.com>
2021-05-12 19:44:31 -0500

    cap fps to 60

--- src/main.cpp
+++ src/main.cpp
@@ -21,12 +21,25 @@ int main(int argc, char* argv[])
     Game* game = new Game();
     game->init(window);
 
+    const int FPS = 60;
+    const int frameDel = 1000/FPS;
+
+    Uint32 frameStart;
+    int frameTime;
+
     char runnning = 1;
     while(runnning)
     {
+        frameStart = SDL_GetTicks();
+
         runnning = game->handleEvents();
         game->update();
         game->render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if(frameDel > frameTime)
+            SDL_Delay(frameDel - frameStart);
     }
 
     return 0;
